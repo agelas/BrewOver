6cc556c17370136d1487aaadbfca49df28a94deb
Mathias <mathiaswi177@gmail.com>
2023-09-03 13:43:14 -0700

    Add tailwind configs and public folder

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: ["./src/**/*.svelte"],
+    theme: {
+      extend: {
+        fontFamily: {
+          primary: 'Playfair Display',
+          body: 'Work Sans',
+        },
+        colors: {
+          primary: '#050402',
+          secondary: '#1C1D24',
+          tertiary: '#131419',
+          accent: {
+            DEFAULT: '#ac6b34',
+            hover: '#925a2b',
+          },
+          paragraph: '#878e99',
+        },
+      },
+    },
+    variants: {},
+    plugins: [],
+  }
