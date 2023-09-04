67b608fe6f61a3e889f4d2bd2c8936769256a806
Mathias <mathiaswi177@gmail.com>
2023-09-04 01:04:14 -0700

    Add some blurring in

--- src/Home.svelte
+++ src/Home.svelte
@@ -3,26 +3,37 @@
         <h1 class="text-[60px] font-primary">BrewOver</h1>
         <p class="text-xl font-body mt-4">A place for coffee.</p>
     </div>
-    <div class="flex-1 flex items-center justify-center">
+    <div class="coffee-img-container">
         <img src="/pourover_image.png" alt="Pourover Coffee Setup" class="coffee-img">
     </div>
 </section>
 
 <style>
+    .coffee-img-container {
+        position: relative;
+        width: 50%; /* Set the width here */
+        overflow: hidden;
+        border-radius: 20px;
+        display: flex; /* Convert to flex container */
+        align-items: center; /* Center vertically */
+        justify-content: center; /* Center horizontally */
+    }
+
    .coffee-img {
-        width: 35%;
+        width: 50%;
         height: auto;
         border-radius: 5px;
     }
 
-    .coffee-img::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-    background: radial-gradient(circle, transparent, #000 80%); /* Adjust gradient as needed */
-    pointer-events: none;
-}
+     .coffee-img-container::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: radial-gradient(circle, transparent, #050402 45%);
+        pointer-events: none;
+    }
+
 </style>
