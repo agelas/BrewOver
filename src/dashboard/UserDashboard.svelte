49c893bc08a5ad8b769af56fa8ddf8bee0233457
Mathias <mathiaswi177@gmail.com>
2023-10-12 23:12:22 -0700

    Start on UserDashboard and UserHeader

---
+++ src/dashboard/UserDashboard.svelte
@@ -0,0 +1,8 @@
+<script>
+    import UserHeader from "./UserHeader.svelte";
+</script>
+
+<div>
+    <UserHeader />
+    /* Content for later */
+</div>
---
+++ src/dashboard/UserHeader.svelte
@@ -0,0 +1,22 @@
+<script>
+    import { navigate } from "svelte-routing/src/history";
+</script>
+
+<section id="user-header" class="user-header">
+    <div class="header-items">
+        /* Display username here */
+        <button on:click={() => navigate('/')}>Logout</button>
+    </div>
+</section>
+
+<style>
+    .user-header {
+        padding: 20px;
+        justify-content: flex-end; /* should hopefully smush everything right */
+    }
+
+    .user-header .header-items {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
