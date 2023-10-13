6a8b8feba32c0c1ffba468a93a446d05cb509c55
Mathias <mathiaswi177@gmail.com>
2023-10-12 23:12:08 -0700

    Add routing to App

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <script lang="ts">
+	import { Route, Router, Link } from "svelte-routing";
 	import Header from "./Header.svelte";
 	import Home from "./Home.svelte";
 	import About from "./About.svelte";
 	import RegisterForm from "./RegisterForm.svelte";
 </script>
+
+<Router>
+	<Route path="/" component={Home} />
+</Router>
 
 <main>
 	<Header />
