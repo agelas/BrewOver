c1a1ba339206ab3833923bc5087e807daca48dad
Mathias <mathiaswi177@gmail.com>
2023-10-14 18:45:03 -0700

    Get username

--- src/dashboard/UserDashboard.svelte
+++ src/dashboard/UserDashboard.svelte
@@ -1,8 +1,23 @@
-<script>
+<script lang="ts">
+    import { onMount } from "svelte";
     import UserHeader from "./UserHeader.svelte";
+    import { supabase } from "../supabaseClient";
+
+    export let username; // Expose the prop to get it from the route
+
+    onMount(async () => {
+        const { data, error } = await supabase.from('Users').select('*').eq('username', username);
+
+        if (error) {
+            console.error('Error fetching user: ', error.message, error.details);
+        } else {
+            username = data[0].username;
+        }
+
+    });
 </script>
 
 <div>
-    <UserHeader />
+    <UserHeader {username} />
     /* Content for later */
 </div>
